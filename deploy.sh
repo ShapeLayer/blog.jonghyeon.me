976ffdae22273440823b53937a9d134569d83076
Park, Jonghyeon <belline0124@gmail.com>
2023-05-13 13:08:16 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 theme=$(awk '/^theme/{print $3}' tools.conf)
 theme_repo_url=$(awk '/^theme_repo_url/{print $3}' tools.conf)
 sh tools/clone_theme_customizer.sh
-mv -rf customizer/deploy .
+cp -rf customizer/deploy .
+rm -rf customizer
