5746a64fec9339a4734d5c1e9af8b644b1e15cf6
Park, Jonghyeon <belline0124@gmail.com>
2023-05-13 14:21:42 +0000

    Update deploy configuration

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 theme=$(awk '/^theme/{print $3}' tools.conf)
 theme_repo_url=$(awk '/^theme_repo_url/{print $3}' tools.conf)
 sh tools/clone_theme_customizer.sh
-cp -rf customizer/deploy/* .
+cp -rf customizer/deploy docs
 rm -rf customizer
