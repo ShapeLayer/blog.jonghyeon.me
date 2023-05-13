ab71389ee9a32591d33d008bd9cf6a593c5f0d83
Park, Jonghyeon <belline0124@gmail.com>
2023-05-13 14:36:37 +0000

    Rollback

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 theme=$(awk '/^theme/{print $3}' tools.conf)
 theme_repo_url=$(awk '/^theme_repo_url/{print $3}' tools.conf)
-sh tools/clone_theme_customizer.sh
-cp -rf customizer/deploy docs
-rm -rf customizer
-mv docs/assets/lib/.gitkeep docs/assets/lib/.gitmodules
-rm docs/.gitmodules
+# sh tools/clone_theme_customizer.sh
+# cp -rf customizer/deploy docs
+# rm -rf customizer
+# mv docs/assets/lib/.gitkeep docs/assets/lib/.gitmodules
+# rm docs/.gitmodules
+sh tools/merge_with_theme.sh $theme $theme_repo_url
