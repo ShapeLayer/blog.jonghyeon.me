f0a1ba22a99dfb963bcdd26a5238650c092043bd
Park, Jonghyeon <belline0124@gmail.com>
2023-05-26 15:28:43 +0900

    Apply new theme repository to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
-theme=$(awk '/^theme/{print $3}' tools.conf)
-theme_repo_url=$(awk '/^theme_repo_url/{print $3}' tools.conf)
-theme_repo_branch=$(awk '/^theme_repo_branch/{print $3}' tools.conf)
+theme=$(awk '/^theme/{print $3}' deploy.conf)
+theme_repo_url=$(awk '/^theme_repo_url/{print $3}' deploy.conf)
+theme_repo_branch=$(awk '/^theme_repo_branch/{print $3}' deploy.conf)
 theme_clone_cache=theme
+theme_path=$theme_clone_cache/"$theme"_deploy
 
 echo 'Pulling theme repository.'
 git clone -b $theme_repo_branch $theme_repo_url $theme_clone_cache
 echo 'Pulling done.'
-echo 'Merging Start.'
-rm -rf $theme_clone_cache/deploy/.git
-cp -rf $theme_clone_cache/deploy/* .
-rm -rf $theme_clone_cache
-echo 'Merging End.'
+if [ -d $theme_path ]; then
+  echo 'Merging Start.'
+  rm -rf $theme_path/deploy/.git
+  cp -rf $theme_path/deploy/* .
+  rm -rf $theme_path
+  echo 'Merging End.'
+else
+  echo 'Theme not found.'
+fi
