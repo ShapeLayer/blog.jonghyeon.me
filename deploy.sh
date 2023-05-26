c2717787a20b86511220ee0a7345214c3ffaf2ac
Park, Jonghyeon <belline0124@gmail.com>
2023-05-26 16:03:27 +0900

    Add missed code

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ echo 'Pulling theme repository...'
 git clone -b $theme_repo_branch $theme_repo_url $theme_clone_cache
 echo 'Pulling done.'
 
+echo "Theme selected: $theme_name ($theme_path)"
 if [ -d $theme_path ]; then
   echo 'Merging Start...'
   rm -rf $theme_path/.git
