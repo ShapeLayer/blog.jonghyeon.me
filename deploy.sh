fa353db113c1e0fe7b66405c35aab79133b2cee1
Park, Jonghyeon <belline0124@gmail.com>
2023-05-26 15:52:37 +0900

    Pull assets before rebase; fix deploy script cannot apply pulled theme

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
-theme=$(awk '/^theme/{print $3}' deploy.conf)
+theme_name=$(awk '/^theme_name/{print $3}' deploy.conf)
 theme_repo_url=$(awk '/^theme_repo_url/{print $3}' deploy.conf)
 theme_repo_branch=$(awk '/^theme_repo_branch/{print $3}' deploy.conf)
 theme_clone_cache=theme
-theme_path=$theme_clone_cache/"$theme"_deploy
+theme_path=$theme_clone_cache/"$theme_name"_deploy
 
-echo 'Pulling theme repository.'
+echo 'Pulling theme repository...'
 git clone -b $theme_repo_branch $theme_repo_url $theme_clone_cache
 echo 'Pulling done.'
+
 if [ -d $theme_path ]; then
-  echo 'Merging Start.'
-  rm -rf $theme_path/deploy/.git
-  cp -rf $theme_path/deploy/* .
+  echo 'Merging Start...'
+  rm -rf $theme_path/.git
+  cp -rf $theme_path/* .
   rm -rf $theme_path
   echo 'Merging End.'
 else
